7b250b620c043a9bb572e6518e991459b3e29b60
merndeveloper0 <pranay.technocomet@gmail.com>
2025-08-08 19:15:01 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,26 +4,27 @@ import { Container, Row, Col } from "react-bootstrap";
 import { setHeaderData } from "@/utils/redux/slices/headerSlice";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
-
+import Logo from "@/assets/images/maia.png";
 
 export default function Home() {
   const dispatch = useDispatch();
 
-   useEffect(() => {
+  useEffect(() => {
     dispatch(setHeaderData({ title: "Home", subtitle: "Welcome to Maiacare" }));
   }, [dispatch]); // ✅ Run once after initial render
 
   return (
     <Container>
-      <Row>
-        <Col xs={6} className="bg-success">
-          <h1>Maicare</h1>
-          {/* What is meaing of Maicare Bro !!!  It's Maiacare */}
-        </Col>
-        <Col xs={6} className="bg-danger">
-          <h1>Doctor</h1>
-        </Col>
-      </Row>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+        }}
+      >
+        <img src={Logo.src} alt="logo" />
+      </div>
     </Container>
   );
 }
