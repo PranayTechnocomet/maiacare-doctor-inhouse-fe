411a508c4efeca56ea10b1d95c336118b2f5c250
Rutvidesai999 <rutvidesaitechnocomet@gmail.com>
2025-08-19 18:42:37 +0530

    chnages

--- src/app/appointments/page.tsx
+++ src/app/appointments/page.tsx
@@ -4,6 +4,7 @@ import { AppDispatch } from "@/utils/redux/store";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { setHeaderData } from "@/utils/redux/slices/headerSlice";
+import AppointmentPatientDetailPage from "@/components/AppointmentPatientDetailPage";
 
 function Page() {
   const dispatch: AppDispatch = useDispatch();
@@ -11,7 +12,10 @@ function Page() {
     dispatch(setHeaderData({ title: "Appointments", subtitle: "Appointments List" }));
   }, []);
   return (
-    <div>Appointments</div>
+    <div>
+      {/* Pass the data to the component */}
+      <AppointmentPatientDetailPage />
+    </div>
   )
 }
 
