7ef80f60188cab30e53721c8286ba4540a4b6543
Rutvidesai999 <rutvidesaitechnocomet@gmail.com>
2025-11-13 15:43:45 +0530

    afternoon code

--- src/utils/apis/apiHelper.tsx
+++ src/utils/apis/apiHelper.tsx
@@ -3,44 +3,55 @@ import apiClient from "./axiosInstance";
 
 export const login = (data: LoginRequest) => {
   return apiClient.post("/auth/login", data);
-} 
+}
 
 export const getLoginUser = () => {
   return apiClient.get("/profile/get/login-user");
 }
 
-export const forgotPassword = (data: {email: string}) => {
+export const forgotPassword = (data: { email: string }) => {
   return apiClient.post("/auth/forgot-password", data);
 }
 
-export const forgotPasswordVerify = (data: {token: string | null, otp: number | string}) => {
+export const forgotPasswordVerify = (data: { token: string | null, otp: number | string }) => {
   return apiClient.post("/auth/forgot-password-verify", data);
 }
 
-export const newPassword = (data: {token: string|null, password: string}) => {
+export const newPassword = (data: { token: string | null, password: string }) => {
   return apiClient.post("/auth/new-password", data);
 }
 
-export const changePassword = (data: {oldPassword: string, newPassword: string}) => {
+export const changePassword = (data: { oldPassword: string, newPassword: string }) => {
   return apiClient.post("/profile/change-password", data);
 }
 
-export const getLoggedInDevice = (data: {token: string|null}) => {
-  return apiClient.post("/profile/list-login-device", {} , {
-    headers : {
+export const getLoggedInDevice = (data: { token: string | null }) => {
+  return apiClient.post("/profile/list-login-device", {}, {
+    headers: {
       Authorization: `Bearer ${data.token}`,
     }
   });
 }
 
+
 export const getLoggedInUser = () => {
   const token = localStorage.getItem("token");
   return apiClient.get("/profile/get", {
-    headers : {
+    headers: {
       Authorization: `Bearer ${token}`,
     }
   });
 }
+
+export const update = () => {
+  const token = localStorage.getItem("token");
+  return apiClient.put("/profile/update", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  });
+}
+
 type QualificationType = {
   degree: string;
   fieldOfStudy: string;
@@ -49,11 +60,76 @@ type QualificationType = {
   endYear: number;
 };
 
-export const addQualification = (data:QualificationType) => {
+export const addQualification = (data: QualificationType) => {
   const token = localStorage.getItem("token");
   return apiClient.post("/profile/qualifications/add", data, {
-    headers : {
+    headers: {
       Authorization: `Bearer ${token}`,
     }
   });
 }
+
+
+export const editQualification = (data: QualificationType) => {
+  const token = localStorage.getItem("token");
+  return apiClient.put("/profile/qualifications/edit", data, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  });
+}
+
+export const deleteQualification = () => {
+  const token = localStorage.getItem("token");
+  return apiClient.delete("/profile/qualifications/delete", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const uploadkycdetails = () => {
+  const token = localStorage.getItem("token");
+  return apiClient.delete("/profile/uploadkycdetails", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const getAll = () => {
+  return apiClient.post("/patient/getAll");
+};
+
+export const patientdelete = () => {
+  const token = localStorage.getItem("token");
+  return apiClient.delete("/patient/delete", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+
+export const addphysicalassessment = () => {
+  const token = localStorage.getItem("token");
+  return apiClient.post("/patient/physical-assessment", {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+export const basicDetails = () => {
+  return apiClient.post("/patient/partner/basicDetails");
+};
